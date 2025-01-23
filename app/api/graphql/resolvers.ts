0e60d69d4d27eab137bdf60d4276b3832790f759
techwithtwin <realwebtechguru@gmail.com>
2025-01-23 13:33:36 +0300

    initialize graphQL

---
+++ app/api/graphql/resolvers.ts
@@ -0,0 +1,7 @@
+const resolvers = {
+  Query: {
+    me: () => 'me',
+  },
+}
+
+export default resolvers
---
+++ app/api/graphql/schema.ts
@@ -0,0 +1,7 @@
+export const schema = `#graphql
+    type Query {
+        me: String
+    }
+`
+
+export default schema
